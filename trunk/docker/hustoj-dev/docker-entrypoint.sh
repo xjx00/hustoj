2d321f6e853a72708a7c9ed6db9a4c68eb86a1cd
shiningrise <shiningrise@gmail.com>
2018-02-28 23:19:31 +0800

    debug docker

--- trunk/docker/hustoj-dev/docker-entrypoint.sh
+++ trunk/docker/hustoj-dev/docker-entrypoint.sh
@@ -4,6 +4,8 @@ DIRECTORY="/data/data/"
 if [ ! -d $DIRECTORY ]; then
 	cp -R /home/judge/data/ /data/
 fi
+chmod 775 -R /data/data 
+chgrp -R www-data /data/data
 mount --bind $DIRECTORY /home/judge/data
 
 DIRECTORY="/data/etc/"
@@ -16,29 +18,24 @@ DIRECTORY="/data/upload"
 if [ ! -d $DIRECTORY ]; then
 	cp -R  /home/judge/src/web/upload /data
 fi
+chmod 770 -R 		/data/upload
+chgrp -R www-data 	/data/upload  
 mount --bind $DIRECTORY /home/judge/src/web/upload
 
 DIRECTORY="/data/config"
 if [ ! -d $DIRECTORY ]; then
 	cp -R  /home/judge/src/web/config /data
 fi
+chmod 770 -R 		/data/config 
+chgrp -R www-data 	/data/config 
 mount --bind $DIRECTORY /home/judge/src/web/config
 
 DIRECTORY="/data/mysql"
 if [ ! -d $DIRECTORY ]; then
 	cp -R /var/lib/mysql /data
 fi
-mount --bind $DIRECTORY /var/lib/mysql
-
-if [  -f "/data/judge.conf" ]; then
-	mv /data/judge.conf /data/etc
-fi
-
-chmod 775 -R /data/data 
-chgrp -R www-data /data/data
-chmod 770 -R 		/data/upload /data/config 
-chgrp -R www-data 	/data/upload /data/config 
 chown -R mysql:mysql /data/mysql/
+mount --bind $DIRECTORY /var/lib/mysql
 
 service mysql start
 /usr/bin/judged
