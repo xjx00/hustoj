2fada7dd3a0e6e50980202fd0f8b1e26433ef82d
shiningrise <shiningrise@gmail.com>
2018-03-11 10:54:29 +0800

    本地 docker build -f Dockerfile -t hustoj  ./

--- trunk/docker/hustoj/build-dokcer.sh
+++ trunk/docker/hustoj/build-dokcer.sh
@@ -1,10 +1,11 @@
 
-#docker rm -f -v /hustoj-dev
-#docker build -f Dockerfile -t hustoj-cpp  ./
+docker build -f Dockerfile -t hustoj  ./
+pause
 
-docker rm -f -v /hustoj-cpp 
+#docker rm -f -v /hustoj-dev
+#docker rm -f -v /hustoj-cpp 
 #docker run -d -it --privileged --name hustoj-cpp -p 8080:80 hustoj-cpp
-docker rm -f /hustoj-cpp 
-docker run -d -it --privileged --name hustoj-cpp -p 8080:80 -v /home/hustoj-cpp/:/data/ hustoj-cpp
+#docker rm -f /hustoj-cpp 
+#docker run -d -it --privileged --name hustoj-cpp -p 8080:80 -v /home/hustoj-cpp/:/data/ hustoj-cpp
 #docker exec -i -t hustoj-cpp /bin/bash
 
